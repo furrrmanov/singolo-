d363b53b788398bbf35669c4c7a481b54c99e2bf
furrrmanov <pokerzatashil@mail.ru>
2020-03-19 01:27:20 +0300

    feat: fixed slider

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ phones.forEach((el)=>{
 })
 
 
-let Left = document.querySelector(".arrow-left");
+/*let Left = document.querySelector(".arrow-left");
 let Right = document.querySelector(".arrow-right");
 
 var slideIndex = 1;
@@ -26,10 +26,11 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++) {
+  for ( i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
+  
 }
 
 Left.onclick = function() {
@@ -40,7 +41,65 @@ Left.onclick = function() {
 Right.onclick = function() {
   plusSlides(1);
   console.log("right");
-};
+};*/
+
+/*  animation slider____________________________ */
+
+let items = document.querySelectorAll('.slide');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n){
+  currentItem = (n + items.length) % items.length
+}
+
+function hideItem (direction) {
+  isEnabled = false;
+  items[currentItem].classList.add(direction)
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('active',direction)
+  });
+}
+
+function showItem (direction) {
+  items[currentItem].classList.add('next', direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('next',direction)
+    this.classList.add('active')
+    isEnabled = true;
+  })
+}
+
+
+
+function previousItem (n) { 
+  hideItem('to-right');
+  changeCurrentItem(n - 1)
+  showItem('from-left')
+}
+
+document.querySelector('.arrow-left').addEventListener('click', function () {
+  if(isEnabled){
+    previousItem(currentItem)
+  }
+})
+
+
+function nextItem (n) { 
+  hideItem('to-left')
+  changeCurrentItem(n + 1)
+  showItem('from-right')
+}
+
+
+document.querySelector('.arrow-right').addEventListener('click', function () {
+  if(isEnabled){
+    nextItem(currentItem)
+  }
+})
+
+
+
 
 
 
